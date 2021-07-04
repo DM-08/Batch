bc0ee429cf105b856da9f975c0675c671b64a943
DM-08 <56652167+DM-08@users.noreply.github.com>
2021-07-04 20:05:56 +0100

    first file

---
+++ GenChars.bat
@@ -0,0 +1,19 @@
+@echo off
+rem
+setlocal enabledelayedexpansion
+set "string=abcdefghijklmnopqrstuvwxyz"
+:start
+set "result="
+set /p WordLen="Enter ID: "
+
+for /L %%i in (1,1,%WordLen%) do call :add
+
+rem Output to text file
+echo %WordLen% >> Word_Length.txt
+echo %result% >> Word_Length.txt
+goto :start
+
+:add
+set /a x=%random% %% 26
+set result=%result%!string:~%x%,1!
+goto :eof
